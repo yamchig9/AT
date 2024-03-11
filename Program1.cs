40e2508fd69a92d2ab516f0d0a1ca32c7382f0c4
Данил Ямов <yamchig9@gmail.com>
2024-03-11 18:55:18 +0700

    task 1

---
+++ Program1.cs
@@ -0,0 +1,13 @@
+﻿int M = 1;
+int N = 10;
+
+PrintNumbersBetween(M, N);
+
+void PrintNumbersBetween(int m, int n)
+{
+    if (m <= n)
+    {
+        Console.Write(m + " ");
+        PrintNumbersBetween(m + 1, n);
+    }
+}
